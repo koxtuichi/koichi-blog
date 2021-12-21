357f54053b0a2fb91a6818d0f541d7aad35a0446
koxtuichi <entertainment1nSugimoto@gmail.com>
2021-12-22 02:27:17 +0900

    インスタリンク追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { Post, getPosts, getDatabaseData, PostIndex, getPostIndex } from '@/lib/util/notion';
 import Head from "next/head";
 import React from 'react';
+import Link from 'next/link'
 
 export const getStaticProps: GetStaticProps<{ posts: Post[], postsIndex: PostIndex[] }> = async () => {
     const database = await getDatabaseData();
@@ -27,7 +28,7 @@ const Index = ({ posts, postsIndex }: InferGetStaticPropsType<typeof getStaticPr
                 <meta name="twitter:card" content="summary" />
                 <meta name="twitter:image" content="https://diary.unronritaro.net/top.png" />
             </Head>
-            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", color: "#212529", fontSize: 16 }}>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", color: "#212529", fontSize: 16, whiteSpace: "nowrap" }}>
                 <img src="/head.jpg" style={{ margin: "24px auto", width: "60vw" }} />
                 <div style={{ marginBottom: 16 }}>SHINJI SUGIMOTO</div>
                 <div style={{ marginBottom: 16 }}>1998/8/4</div>
@@ -35,13 +36,18 @@ const Index = ({ posts, postsIndex }: InferGetStaticPropsType<typeof getStaticPr
                 <div style={{ marginBottom: 16 }}>---</div>
                 <div style={{ marginBottom: 16 }}>Contact</div>
                 <div style={{ marginBottom: 16 }}>sugimotoshinji11@gmail.com</div>
-                <div style={{ marginBottom: 16 }}>Instagram</div>
+                <div style={{ marginBottom: 16, display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <Link href={'https://www.instagram.com/shinjisugimoto84/?igshid=1eh2ijp64yeun'}>
+                        <img src="/insta.png" style={{ width: 30, height: 30, cursor: "pointer" }} />
+                    </Link>
+                    <div>Instagram</div>
+                </div>
                 <div style={{ marginBottom: 16 }}>---</div>
 
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around", flexWrap: "wrap", margin: "auto 24px", width: "80vw" }}>
                 {posts.sort((a, b) => a.num - b.num).map((post, i) => (
                     <React.Fragment>
-                        <img src={post.url} style={{ height: 290, margin: "0 8px 12px 8px" }} />
+                        <img src={post.url} style={{ maxHeight: 290, margin: "0 8px 12px 8px" }} />
                     </React.Fragment>
                 ))}
                 </div>

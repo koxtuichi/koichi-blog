78153489a130bca0bc06fa40c994a53c87f34c63
koxtuichi <entertainment1nsugimoto@gmail.com>
2022-11-26 16:19:10 +0900

    データの取得方法を変更

--- lib/util/notion.ts
+++ lib/util/notion.ts
@@ -1,5 +1,5 @@
 import { Client } from '@notionhq/client';
-import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+import { isFullPage } from '@jitl/notion-api';
 
 export type PostIndex = {
     id: string,
@@ -23,11 +23,6 @@ export type Content = {
 export type Post = {
     id: string,
     title: string,
-    date: string,
-    ymd: string,
-    createdTs: string,
-    lastEditedTs: string,
-    contents: Content[],
     url: any,
     num: any,
 }
@@ -36,104 +31,29 @@ const notion = new Client({
     auth: process.env.NOTION_SECRET,
 });
 
-export const getDatabaseData = async (
-    // startCursor?: string, pageSize?: number
-) => {
-    return notion.databases.query({
-        database_id: process.env.NOTION_DATABASE_ID || '',
-        filter: {
-            or: [
-                {
-                    property: 'published',
-                    checkbox: {
-                        equals: true,
-                    },
-                },
-            ]
-        },
-        sorts: [
-            {
-                property: 'date',
-                direction: 'descending',
-            },
-        ],
-        // start_cursor: startCursor,
-        // page_size: pageSize
-    });
-};
+export const getPageDatas = async () => {
+	const fullOrPartialPages = await notion.databases.query({
+		database_id: process.env.NOTION_DATABASE_ID || "",
+	});
 
+	const posts: Post[] = [];
 
-export const getPosts = async (databaseResponse: QueryDatabaseResponse, ids?: string[]) => {
-    const postContentPromises = [];
-    if (ids) {
-        ids.forEach(id => {
-            postContentPromises.push(notion.blocks.children.list({ block_id: id }));
-        });
-    } else {
-        for (const result of databaseResponse.results) {
-            postContentPromises.push(notion.blocks.children.list({ block_id: result.id }));
-        }
-    }
-    const postContents = await Promise.all(postContentPromises);
-    const posts: Post[] = postContents.map((postContent, i) => {
-        const page = ids ? databaseResponse.results.find((result) => result.id === ids[i]) : databaseResponse.results[i];
-        // @ts-ignore
-        const date: string = page.properties.date.date.start;
-        const image: any = page?.properties.image;
-        const num: any = page?.properties.num;
-        const post: Post = {
-            id: page?.id || '',
-            // @ts-ignore
-            title: page?.properties.post.title[0].plain_text,
-            date,
-            ymd: date.replace(/-/g, ''),
-            createdTs: page?.created_time || '',
-            lastEditedTs: page?.last_edited_time || '',
-            contents: [],
-            url: image.files[0].file.url || '',
-            num: num.number,
-        }
-        postContent.results.forEach((result) => {
-            switch (result.type) {
-                case 'paragraph': post.contents.push({
-                    type: result.type,
-                    text: result['paragraph'].text[0]?.plain_text || null,
-                    link: result['paragraph'].text[0]?.href || null,
-                });
-                    break;
-                case 'quote': post.contents.push({
-                    type: result.type,
-                    text: result['quote'].text[0]?.plain_text || null,
-                    link: result['quote'].text[0]?.href || null,
-                });
-                    break;
-                case 'code': post.contents.push({
-                    type: result.type,
-                    text: result['code'].text[0]?.plain_text || null,
-                    link: result['code'].text[0]?.href || null,
-                    language: result['code'].language || null,
-                });
-                    break;
-            }
-        });
-        return post;
-    });
-    return posts;
+	for (const page of fullOrPartialPages.results) {
+		if (!isFullPage(page)) {
+			continue
+		}
+		const post: any = page.properties.post;
+		const image: any = page.properties.image;
+		const num: any = page.properties.num;
+		posts.push(
+			{
+				id: page.id,
+				title: post.title[0].plain_text,
+				url: image.files[0]?.file?.url || "",
+				num: num.number,
+			}
+		);
+	}
+
+	return posts;
 }
-
-export const getPostIndex = (response: QueryDatabaseResponse) => {
-    const postIndex: PostIndex[] = response.results.map((result) => {
-        // @ts-ignore
-        const ymd = (result.properties.date.date.start as string).replace(/-/g, '');
-        return {
-            id: result.id,
-            // @ts-ignore
-            title: result.properties.post.title[0].text.content as string,
-            ymd,
-            year: ymd.substring(0, 4),
-            month: ymd.substring(4, 6),
-            date: ymd.substring(6, 8),
-        }
-    });
-    return postIndex;
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
-import { Post, getPosts, getDatabaseData, PostIndex, getPostIndex } from '@/lib/util/notion';
+import { Post, getPageDatas } from '@/lib/util/notion';
 import Head from "next/head";
 import React, { useState } from 'react';
 import Link from 'next/link'
 import { Dialog, DialogContent } from '@material-ui/core';
+import Image from 'next/image';
 
 export const getStaticProps: GetStaticProps<{ posts: Post[] }> = async () => {
-    const database = await getDatabaseData();
-    // console.dir(database, { depth: null })
-    const posts = await getPosts(database);
+    const posts = await getPageDatas();
     return {
         props: {
             posts: posts,
@@ -29,7 +28,7 @@ const Index = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
                 <meta name="twitter:image" content="/public/shinjiPhotoMainImg.jpg" />
             </Head>
             <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", color: "#212529", fontSize: 16, whiteSpace: "nowrap" }}>
-                <img src="/head.jpg" style={{ margin: "24px auto", width: "60vw" }} />
+                <img src="/head.jpg" style={{ margin: "24px auto", width: "60vw" }} alt="head"/>
                 <div style={{ marginBottom: 16 }}>SHINJI SUGIMOTO</div>
                 <div style={{ marginBottom: 16 }}>1998/8/4</div>
                 <div style={{ marginBottom: 16 }}>Born in Mie, Japan.</div>
@@ -37,7 +36,7 @@ const Index = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
                 <div style={{ marginBottom: 16 }}>Contact</div>
                 <div style={{ marginBottom: 16 }}>sugimotoshinji11@gmail.com</div>
                 <div style={{ marginBottom: 16, display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    <Link href={'https://www.instagram.com/shinjisugimoto84/?igshid=1eh2ijp64yeun'}>
+                    <Link href={'https://www.instagram.com/shinjisugimoto84/?igshid=1eh2ijp64yeun'} passHref>
                         <img src="/insta.png" style={{ width: 30, height: 30, cursor: "pointer" }} alt="insta"/>
                     </Link>
                     <div>Instagram</div>
@@ -45,10 +44,8 @@ const Index = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
                 <div style={{ marginBottom: 16 }}>---</div>
 
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around", flexWrap: "wrap", margin: "auto 24px", width: "80vw" }}>
-                {posts.sort((a, b) => a.num - b.num).map((post, i) => (
-                    <React.Fragment key={i}>
-                        <img src={post.url} style={{ maxHeight: 290, margin: "0 8px 12px 8px", cursor: "pointer" }} onClick={() => openPhoto(post)} />
-                    </React.Fragment>
+                {posts && posts.sort((a, b) => a.num - b.num).map((post, i) => (
+                    <img key={i} style={{ maxHeight: 290, height: "100%", margin: "0 8px 12px 8px", cursor: "pointer" }} src={post.url} onClick={() => openPhoto(post)} alt={post.title} />
                 ))}
                 </div>
             </div>
@@ -57,7 +54,7 @@ const Index = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
                 onClose={() => openPhoto(null)}
                 PaperProps={{ style:{ padding: 4 } }}>
                 <DialogContent style={{ padding: 0 }}>
-                    <img src={selectedPhoto && selectedPhoto.url} style={{ overflow: "hidden" }} onClick={() => openPhoto(null)} />
+                    <img src={selectedPhoto && selectedPhoto.url} style={{ overflow: "hidden" }} onClick={() => openPhoto(null)} alt={selectedPhoto && selectedPhoto.title} />
                 </DialogContent>
             </Dialog>
         </div>

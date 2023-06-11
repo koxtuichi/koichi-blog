731fc1004d5860f948f6a2da0642fd4a9b4e6498
杉本光一 <sugimotokouichi@sugimotokouichinoMacBook-Pro.local>
2023-06-11 17:05:42 +0900

    自己紹介文を変更

--- component/home/profile.tsx
+++ component/home/profile.tsx
@@ -10,8 +10,7 @@ const Profile: React.FC<profileProps> = ({ viewEng }) => {
         <>
           <p>1992/9/14生まれ。</p>
           <p>
-            SIGMA fpとレンズはSteinheil MunchenのCassaron 40mm
-            f3.5を使っている。
+            Steinheil MunchenのCassaron 40mm f3.5を使っている。
           </p>
           <p>ここに上げてる写真すべて。</p>
         </>
@@ -19,7 +18,7 @@ const Profile: React.FC<profileProps> = ({ viewEng }) => {
       {viewEng && (
         <>
           <p>Born on 9/14/1992.</p>
-          <p>I use SIGMA fp and a Steinheil Munchen Cassaron 40mm f3.5 lens.</p>
+          <p>A Steinheil Munchen Cassaron 40mm f3.5 lens.</p>
           <p>All the photos I&apos;m posting here.</p>
         </>
       )}

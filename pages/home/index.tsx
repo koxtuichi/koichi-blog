403e874c6c39b83b1ddf44410fbf8b6c2fefb54a
杉本光一 <sugimotokouichi@sugimotokouichinoMacBook-Pro.local>
2023-06-19 00:37:32 +0900

    修正

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -81,23 +81,35 @@ const Home = ({
         <Form onSubmit={handleSubmit}>
           <Grid centered>
             <ContainerCenter>
-              <p style={{ marginBottom: '8p' }}>どんな写真が撮りたいですか？<br />「お花」や「カフェ」などの撮りたいモノの名前を10文字以内で入れてみてください。<br />関東でおすすめのスポットをAIが探してくれます。</p>
-              <Input
-                type="input"
-                name="searchWord"
-                onChange={(e) => setPrompt(e.target.value)}
-                value={prompt}
-                loading={loading}
-                maxLength={10}
-              />
-              <Button type="submit" disabled={loading}>探す</Button>
-              {0 < response.split(",").length && (
-                <>
-                  {response.split(",").map((item, index) => (
-                    <p key={index} style={{ marginTop: '8px' }}>{item}</p>
-                  ))}
-                </>
-              )}
+              <p style={{ marginBottom: "8p" }}>
+                どんな写真が撮りたいですか？
+                <br />
+                「お花」や「カフェ」などの撮りたいモノの名前を10文字以内で入れてみてください。
+                <br />
+                関東でおすすめのスポットをAIが探してくれます。
+              </p>
+              <div style={{ display: "flex", maxHeight: "36px" }}>
+                <Input
+                  type="input"
+                  name="searchWord"
+                  onChange={(e) => setPrompt(e.target.value)}
+                  value={prompt}
+                  loading={loading}
+                  maxLength={10}
+                />
+                <Button type="submit" disabled={loading}>
+                  探す
+                </Button>
+                {0 < response.split(",").length && (
+                  <>
+                    {response.split(",").map((item, index) => (
+                      <p key={index} style={{ marginTop: "8px" }}>
+                        {item}
+                      </p>
+                    ))}
+                  </>
+                )}
+              </div>
             </ContainerCenter>
           </Grid>
         </Form>

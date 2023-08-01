d1f9e5fa75fa321892fbc7dd5521eab9495a6e5e
杉本光一 <sugimotokouichi@sugimotokouichinoMacBook-Pro.local>
2023-08-02 00:10:14 +0900

    自動デプロイなし

--- component/home/SecondPost.tsx
+++ component/home/SecondPost.tsx
@@ -33,9 +33,9 @@ const SecondPost: React.FC<SecondPostProps> = ({
           return (
             <GridImageComponent key={index}>
               <ImageComponent
-                // src={`blogImages/${post.id}.png`}
-                  src={post.url}
-                  size="huge"
+                src={`blogImages/${post.id}.png`}
+                  // src={post.url}
+                size="huge"
                 wrapped
                 ui={true}
                 onClick={() => setSelectedPhoto(post)}

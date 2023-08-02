4c3639816b1a5e497c0bd34e2cf0a4cd0427087d
杉本光一 <sugimotokouichi@sugimotokouichinoMacBook-Pro.local>
2023-08-02 23:59:41 +0900

    修正

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,24 @@ import HomeComponent from "./_page";
 
 import fs from 'fs'
 
-export const dynamic = 'error';
+export const dynamic = 'force-dynamic';
 
 const Home = async () => {
-  const checkUrl = async (url: string) => {
-    try {
-      const response = await fetch(url, {
-        method: 'HEAD', // 'HEAD'リクエストはリソースを取得せずにヘッダー情報だけを取得します
-      });
+  // const checkUrl = async (url: string) => {
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: 'HEAD', // 'HEAD'リクエストはリソースを取得せずにヘッダー情報だけを取得します
+  //     });
 
-      // 200 OKのレスポンスが返ってきたら、URLは有効です
-      if (response.ok) {
-        return true;
-      }
-      return false;
-    } catch (error) {
-      return false;
-    }
-  }
+  //     // 200 OKのレスポンスが返ってきたら、URLは有効です
+  //     if (response.ok) {
+  //       return true;
+  //     }
+  //     return false;
+  //   } catch (error) {
+  //     return false;
+  //   }
+  // }
   const posts = await getPageDatas();
   const imagePath = 'public/blogImages';
 
@@ -31,7 +31,6 @@ const Home = async () => {
   posts.map(async (post) => {
     const id = post.id;
     const url = post.url;
-    if (!checkUrl(url)) return;
     const savePath = imagePath + '/' + id + '.png';
     if (fs.existsSync(savePath)) {
       return;

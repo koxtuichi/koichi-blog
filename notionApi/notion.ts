cef842ea7da867f51569bc95e1bd8abb98507acf
杉本光一 <sugimotokouichi@sugimotokouichinoMacBook-Pro.local>
2023-08-04 00:02:10 +0900

    修正

--- notionApi/notion.ts
+++ notionApi/notion.ts
@@ -41,7 +41,7 @@ export const getPageDatas = async () => {
 
     const url = image.files[0]?.file?.url || "";
     const imagePath = 'public/blogImages';
-    const savePath = 'public/' + page.id + '.png';
+    const savePath = '/' + page.id + '.png';
     if (!fs.existsSync(savePath)) {
       const blob = await fetch(url).then((r) => r.blob());
       const binary = (await blob.arrayBuffer()) as Uint8Array;

714c164d05f7a5be3dee85881856acbaeef02717
杉本光一 <sugimotokouichi@sugimotokouichinoMacBook-Pro.local>
2023-08-03 19:36:52 +0900

    修正

--- app/page.tsx
+++ app/page.tsx
@@ -24,28 +24,28 @@ const Home = async () => {
   // }
   try {
     const posts = await getPageDatas();
-    const imagePath = 'public/blogImages';
+    // const imagePath = 'public/blogImages';
 
-    if (!fs.existsSync(imagePath)) {
-      fs.mkdirSync(imagePath)
-    }
+    // if (!fs.existsSync(imagePath)) {
+    //   fs.mkdirSync(imagePath)
+    // }
 
-    posts.map(async (post) => {
-      const id = post.id;
-      const url = post.url;
-      const savePath = imagePath + '/' + id + '.png';
-      if (fs.existsSync(savePath)) {
-        return;
-      }
-      const blob = await fetch(url).then((r) => r.blob());
-      const binary = (await blob.arrayBuffer()) as Uint8Array;
-      const buffer = Buffer.from(binary);
-      fs.writeFile(savePath, buffer, (error) => {
-        if (error) {
-          throw error;
-        }
-      })
-    })
+    // posts.map(async (post) => {
+    //   const id = post.id;
+    //   const url = post.url;
+    //   const savePath = imagePath + '/' + id + '.png';
+    //   if (fs.existsSync(savePath)) {
+    //     return;
+    //   }
+    //   const blob = await fetch(url).then((r) => r.blob());
+    //   const binary = (await blob.arrayBuffer()) as Uint8Array;
+    //   const buffer = Buffer.from(binary);
+    //   fs.writeFile(savePath, buffer, (error) => {
+    //     if (error) {
+    //       throw error;
+    //     }
+    //   })
+    // })
     return (
       <HomeComponent posts={posts} />
     );

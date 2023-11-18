2345559a7fdadb8bababdd5673a012d1de9b666c
杉本光一 <sugimotokouichi@koichimac.local>
2023-11-18 21:28:41 +0900

    typeエラー修正

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,69 +1,69 @@
-import { Client } from "@notionhq/client";
+import { Client } from '@notionhq/client';
 
 const notion = new Client({
-  auth: process.env.NOTION_SECRET,
+	auth: process.env.NOTION_SECRET,
 });
 
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-  try {
-    const response = await request.json()
-    const res = await notion.pages.create({
-      parent: {
-        database_id: process.env.NOTION_CREATE_PRESENT_INFO_ID || "",
-      },
-      properties: {
-        name: {
-          type: "title",
-          title: [
-            {
-              type: "text",
-              text: {
-                content: response?.['submitObj']?.['name'] ?? '',
-              },
-            },
-          ],
-        },
-        zipCode: {
-          type: "rich_text",
-          rich_text: [
-            {
-              type: "text",
-              text: {
-                content: response?.['submitObj']?.['zipCode'] ?? '',
-              },
-            },
-          ],
-        },
-        address: {
-          type: "rich_text",
-          rich_text: [
-            {
-              type: "text",
-              text: {
-                content: response?.['submitObj']?.['address'] ?? '',
-              },
-            },
-          ],
-        },
-        email: {
-          type: "rich_text",
-          rich_text: [
-            {
-              type: "text",
-              text: {
-                content: response?.['submitObj']?.['email'] ?? '',
-              },
-            },
-          ],
-        },
-      },
-    });
+	try {
+		const response = await request.json();
+		const res = await notion.pages.create({
+			parent: {
+				database_id: process.env.NOTION_CREATE_PRESENT_INFO_ID || '',
+			},
+			properties: {
+				name: {
+					type: 'title',
+					title: [
+						{
+							type: 'text',
+							text: {
+								content: response?.['submitObj']?.['name'] ?? '',
+							},
+						},
+					],
+				},
+				zipCode: {
+					type: 'rich_text',
+					rich_text: [
+						{
+							type: 'text',
+							text: {
+								content: response?.['submitObj']?.['zipCode'] ?? '',
+							},
+						},
+					],
+				},
+				address: {
+					type: 'rich_text',
+					rich_text: [
+						{
+							type: 'text',
+							text: {
+								content: response?.['submitObj']?.['address'] ?? '',
+							},
+						},
+					],
+				},
+				email: {
+					type: 'rich_text',
+					rich_text: [
+						{
+							type: 'text',
+							text: {
+								content: response?.['submitObj']?.['email'] ?? '',
+							},
+						},
+					],
+				},
+			},
+		});
 
-    const data = await JSON.stringify(res);
-    return NextResponse.json(data)
-  } catch (e) {
-    console.dir(e);
-  }
+		const data = await JSON.stringify(res);
+		return NextResponse.json(data);
+	} catch (e) {
+		console.dir(e);
+	}
 }

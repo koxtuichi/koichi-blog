a24ea1dc02798b33ef3fab5957e1b7ade8fe093f
杉本光一 <sugimotokouichi@koichimac.local>
2024-03-20 15:06:42 +0900

    SNSにThreadsとXを追加

--- app/_snsIcons.tsx
+++ app/_snsIcons.tsx
@@ -1,23 +1,44 @@
-import { Box } from "@chakra-ui/react";
-import { Grid, Icon } from "semantic-ui-react";
+import { Box, ButtonGroup } from "@chakra-ui/react";
+import { Typography } from "@mui/material";
+import { Grid } from "semantic-ui-react";
 
 const SnsIcons = () => {
+  const SnsLinkDiv = ({
+    name,
+    path,
+  }: {
+    name: string;
+    path: string;
+  }): JSX.Element => {
+    return (
+      <Typography
+        component="div"
+        style={{
+          cursor: "pointer",
+          textDecoration: "underline",
+        }}
+        fontStyle="italic"
+        onClick={() => window.open(path, "_blank")}
+      >
+        {name}
+      </Typography>
+    );
+  };
   return (
     <Box paddingRight="20px">
       <Grid centered>
         <Grid.Row>
-          <Icon
-            name="instagram"
-            size="big"
-            link
-            onClick={() =>
-              window.open(
-                "https://www.instagram.com/kakikuke_koichi/",
-                "_blank"
-              )
-            }
-          />
-          <p style={{ lineHeight: 2 }}>since 2023/2</p>
+          <ButtonGroup gap={4}>
+            <SnsLinkDiv
+              name="Instagram"
+              path="https://www.instagram.com/kakikuke_koichi/"
+            />
+            <SnsLinkDiv
+              name="Threads"
+              path="https://www.threads.net/@kakikuke_koichi"
+            />
+            <SnsLinkDiv name="X" path="https://twitter.com/kakikukekoichi" />
+          </ButtonGroup>
         </Grid.Row>
       </Grid>
     </Box>

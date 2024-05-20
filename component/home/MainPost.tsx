cb7de9d123ac1474617982f8239a0e7693ec8461
杉本光一 <sugimotokouichi@koichimac.local>
2024-05-20 22:48:23 +0900

    改行されるようにする

--- component/home/MainPost.tsx
+++ component/home/MainPost.tsx
@@ -6,7 +6,9 @@ import styled from "@emotion/styled";
 
 import { Image } from "@chakra-ui/react";
 
-const ContainerContent = styled(Card.Content)({});
+const ContainerContent = styled(Card.Content)({
+  whiteSpace: "break-space",
+});
 
 type MainPostProps = {
   posts: Post[];
